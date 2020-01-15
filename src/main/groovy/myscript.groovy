8d0c075fd6438aa75c97997b2402cd34c34b7a9c
Asif Kamran Malick <asifkamran.malick8@gmail.com>
2020-01-15 19:30:30 +0530

    commiting starter script for myscript.groovy

--- src/main/groovy/myscript.groovy
+++ src/main/groovy/myscript.groovy
@@ -1,31 +1,8 @@
-
-// import groovy.io.FileType
-// import java.nio.file.Path
-// import java.nio.file.Files;
-// import java.nio.file.FileAlreadyExistsException
-// import java.io.FileNotFoundException
-// import java.nio.file.Paths;
-
-// def specificFilesPath = new File("../../../symlinkTestFiles/abc.txt")
-
-// def srcPath = specificFilesPath.toPath()
-// println "srcPath is ------------   " + srcPath
-
-// def symLinkToCreate = new File("../../../symlinkTestFiles/symLinkedFile/abc.txt").toPath()
-// println "symLinkToCreate -------------   " + symLinkToCreate
-
-// try{
-// Path symLink = Files.createSymbolicLink(symLinkToCreate, srcPath);
-// println "symlink created -----   " + symLink
-// }catch(FileAlreadyExistsException faee){
-//    println "File Already Exists ! AbsolutePath  :--  " + symLinkToCreate.toAbsolutePath()
-//    println "File Already Exists ! Path  :--  " + symLinkToCreate
-// }
-
-
 /***
 
-// Working Solution : start
+//----------- creates a VALID symlink, butt the symlink when click-opened in github shows absolute path of the original file that this created symlink refers to
+
+// SNIPPET 1 : START
 
 import groovy.io.FileType
 import java.nio.file.Path
@@ -34,36 +11,29 @@ import java.nio.file.FileAlreadyExistsException
 import java.io.FileNotFoundException
 import java.nio.file.Paths;
 
-println "Hello from script"
-
-
-// def specificFilesPath = new File("../../../symlinkTestFiles/abc.txt")
 def specificFilesPath = Paths.get("toSymlink.groovy").toAbsolutePath()
-
-// println specificFilesPath.getAbsolutePath().text;
 
 def srcPath = specificFilesPath.toAbsolutePath()
 println "srcPath is ------------   " + srcPath
-// def symLinkToCreate = new File("../../../symlinkTestFiles/symLinkedFile/abc.txt").toAbsolutePath()
+
 def symLinkToCreate = Paths.get("../../../symlinkTestFiles/toSymlink.groovy").toAbsolutePath()
-
-
 println "symLinkToCreate -------------   " + symLinkToCreate
-// println "location ---------  "+new File("..\\symlinkTestFiles\\symLinkedFile\\abc.txt"   )
-File directory = new File("./");
-println "directory is :---" + directory
-   System.out.println(directory.getAbsolutePath());
+
 try{
 Path symLink = Files.createSymbolicLink(symLinkToCreate, srcPath);
 println "symlink created -----   " + symLink
 }catch(FileAlreadyExistsException faee){
 }
 
-// Working Solution : end
+// SNIPPET 1 : END
 ***/
 
 
+/****
 
+//----------- creates a INVALID symlink, and the symlink when click-opened in github shows absolute path of the original file that this created symlink refers to
+
+// SNIPPET 2 : START
 
 import groovy.io.FileType
 import java.nio.file.Path
@@ -87,3 +57,6 @@ println "symlink created -----   " + symLink.toAbsolutePath()
 }catch(FileAlreadyExistsException faee){
 }
 
+// SNIPPET 2 : END
+
+*****/
