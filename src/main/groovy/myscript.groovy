dcafe97b3fd21cee0d90b5434d95557eb032148b
Asif Kamran Malick <asifkamran.malick8@gmail.com>
2020-01-15 21:41:09 +0530

    Committing changes for myscript.groovy after removing commented code block for invalid symlink creation

--- src/main/groovy/myscript.groovy
+++ src/main/groovy/myscript.groovy
@@ -27,36 +27,3 @@ println "symlink created -----   " + symLink
 
 // SNIPPET 1 : END
 // ***/
-
-
-/****
-
-//----------- creates a INVALID symlink, and the symlink when click-opened in github shows absolute path of the original file that this created symlink refers to
-
-// SNIPPET 2 : START
-
-import groovy.io.FileType
-import java.nio.file.Path
-import java.nio.file.Files;
-import java.nio.file.FileAlreadyExistsException
-import java.io.FileNotFoundException
-import java.nio.file.Paths;
-
-def specificFilesPath = new File("toSymlink.groovy")
-
-def srcPath = specificFilesPath.toPath()
-println "srcPath is ------------   " + srcPath.toAbsolutePath()
-def symLinkToCreate = new File("../../../symlinkTestFiles/toSymlink.groovy").toPath()
-
-
-println "symLinkToCreate -------------   " + symLinkToCreate.toAbsolutePath()
-
-try{
-Path symLink = Files.createSymbolicLink(symLinkToCreate, srcPath);
-println "symlink created -----   " + symLink.toAbsolutePath()
-}catch(FileAlreadyExistsException faee){
-}
-
-// SNIPPET 2 : END
-
-*****/
